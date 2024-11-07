24a4a6041561d0e19a8c0e8b3d90b68128f42261
Lucas Akatsuka <ljoakatsuka@hotmail.com>
2024-11-07 12:56:19 -0300

    terceiro commit

---
+++ src/Controllers/AuthController.js
@@ -0,0 +1,36 @@
+const { ReturnDocument } = require("mongodb");
+const UsuarioModel = require("../Models/UsuarioModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
+class AuthController {
+    async login(req, res) {
+        try {
+            const {email, senha} = req.body;
+
+            const usuarioEncontrado = await UsuarioModel.findOne({ email }).select("+senha");
+
+            if(!usuarioEncontrado) 
+                return res.status(403).json({message: "Email ou senha inválidos"})
+                
+            const ehCorrespondente = await bcrypt.compare(senha, usuarioEncontrado.senha);
+            if (!ehCorrespondente)
+                return res.status(403).json({message: "Email ou senha inválidos"})
+            
+            const {senha: hashedSenha, ...usuario} = usuarioEncontrado.toObject();
+            
+            const token = jwt.sign({
+                usuario
+            }, process.env.JWT_SECRETE, {expiresIn: process.env.JWT_EXPIRE_IN});
+
+            res.status(200).json({token});
+        } catch (error) {
+            res.status(500).json({message: "Deu ruim", error: error.message})
+        }
+    }
+}   
+    
+    
+
+module.exports = new AuthController();
